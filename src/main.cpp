803a9d87e67d3f0bceb7f1c8a535c1187a183789
Guilherme Emeterio <guilhermeemeterioif@gmail.com>
2026-03-10 06:40:56 -0300

    project update

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,69 @@
     const BaseType_t app_core = 1;
 #endif
 
-// Duas tasks: Umas escuta uma mensagem do monitor serial
+// Duas tasks: 
+// Uma escuta uma mensagem do monitor serial
 // Outra envia de volta a mensagem via UART
 // 
+
+const int maxSize = 120;
 
 static bool messageFlag = false;
 
 static char *message = "";
 
 void readSerialTask(void *p) {
-    
+    Serial.println("DEBUG: readSerialTask started");
+    message = (char *)pvPortMalloc(sizeof(char) * maxSize);
+    Serial.println("DEBUG: memory allocated");
+    int i = 0;
+    Serial.println("DEBUG: i initialized to 0");
+    message[0] = '\0';
+    Serial.println("DEBUG: message[0] set to null terminator");
+    while (1) {
+        Serial.println("DEBUG: waiting for serial input");
+        if (Serial.available()) {
+            Serial.println("DEBUG: serial data available");
+            char c = Serial.read();
+            Serial.print("DEBUG: received char: ");
+            Serial.println(c);
+
+            if (c == '\n' || c == '\r') {
+                Serial.println("DEBUG: newline detected, breaking");
+                break;
+            }
+            Serial.println("DEBUG: char is not newline");
+
+            message[i++] = c;
+            Serial.print("DEBUG: char stored at index ");
+            Serial.println(i - 1);
+
+            if(i >= 120) {
+                Serial.println("DEBUG: max size reached, breaking");
+                break;
+            }
+            Serial.println("DEBUG: size within limits");
+
+            vTaskDelay(200);
+            Serial.println("DEBUG: delay completed");
+        }
+    }
+    message[i] = '\0';
+    Serial.println("DEBUG: null terminator added");
+    Serial.print("DEBUG: final message: ");
+    Serial.println(message);
+    if(message[0] != '\0') {
+        messageFlag = true;
+        Serial.println("DEBUG: messageFlag set to true");
+    }
+    Serial.println("DEBUG: readSerialTask ending");
 }
 
 void printToSerialTask(void *p) {
     if(messageFlag) {
-
+        Serial.println(message);
     }
+    vTaskDelay(200);
 }
 
 void setup() {
