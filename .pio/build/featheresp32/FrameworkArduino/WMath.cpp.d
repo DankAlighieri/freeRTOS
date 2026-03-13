ded7c00f90ca76f45c30149a1f4923ea9f8e2431
Guilherme Emetério <guilhermeemeterioif@gmail.com>
2026-03-13 06:24:25 -0300

    Project update

---
+++ .pio/build/featheresp32/FrameworkArduino/WMath.cpp.d
@@ -0,0 +1,18 @@
+.pio/build/featheresp32/FrameworkArduino/WMath.cpp.o: \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/cores/esp32/WMath.cpp \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_system/include/esp_system.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_bit_defs.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_idf_version.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_mac.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_chip_info.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_random.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-log.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_timer/include/esp_timer.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/log/include/esp_log.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_rom/include/esp_rom_sys.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/soc/esp32/include/soc/reset_reasons.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_rom/include/esp32/rom/ets_sys.h \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/log/include/esp_log_internal.h
---
+++ .pio/build/featheresp32/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/featheresp32/FrameworkArduino/cbuf.cpp.o: \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /home/laukim/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
